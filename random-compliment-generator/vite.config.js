10c1dbb6b1712c5ac5c5672d22b5dba3bf72a141
Zulaikha mir <zulaikhamir786@gmail.com>
2026-02-27 06:54:10 +0530

    add CI/CD for random compliment generator
    add CI/CD for random compliment generator
    
    fix path
    
    fix path
    
    fix path
    
    path

---
+++ random-compliment-generator/vite.config.js
@@ -0,0 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/react-mini-labs/",
+});
